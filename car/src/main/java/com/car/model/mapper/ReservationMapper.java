b3dd3a7c9c9febbc8a16370c79d285a1e40fbd77
kyeongjoo <stu22@stu22-PC>
2016-08-10 19:00:50 +0900

    나당하하하

---
+++ car/src/main/java/com/car/model/mapper/ReservationMapper.java
@@ -0,0 +1,12 @@
+package com.car.model.mapper;
+
+import java.util.List;
+
+import com.car.model.dto.Reservation;
+
+public interface ReservationMapper {
+	
+	void insertReservation(Reservation reservation);
+	
+
+}
