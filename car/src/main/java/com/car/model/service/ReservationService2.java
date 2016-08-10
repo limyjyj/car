3c6397acb60dc4283768ad7cf0a3d317b0657ca5
ykkwon91 <stu9@stu9-PC>
2016-08-10 20:07:17 +0900

    yk

---
+++ car/src/main/java/com/car/model/service/ReservationService2.java
@@ -0,0 +1,11 @@
+package com.car.model.service;
+
+import java.util.List;
+
+import com.car.model.dto.Reservation;
+
+public interface ReservationService2 {
+
+	List<Reservation> selectReservationList();
+	
+}
