e7dec6a20cd79edb31c815ff0cdb7f54e6e88173
sm <tomato869@naver.com>
2016-08-10 19:54:01 +0900

    sm

---
+++ car/src/main/java/com/car/model/dto/Member.java
@@ -0,0 +1,89 @@
+package com.car.model.dto;
+
+import java.util.Date;
+
+public class Member {
+
+	private int memberNo;
+	private String passWd;
+	private String name;
+	private String gender;
+	private String phone;
+	private String memberId;
+	private int likes;
+	private boolean deleted;
+	private Date regDate;
+
+	public int getMemberNo() {
+		return memberNo;
+	}
+
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+
+	public String getPassWd() {
+		return passWd;
+	}
+
+	public void setPassWd(String psaaWd) {
+		this.passWd = psaaWd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+}
---
+++ car/src/main/java/com/car/model/service/MemberServiceImpl.java
@@ -0,0 +1,35 @@
+package com.car.model.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.car.model.dao.MemberDao;
+import com.car.model.dto.Member;
+
+@Service("memberService")
+public class MemberServiceImpl implements MemberService {
+
+	@Autowired
+	@Qualifier("oracleMemberDao")
+	private MemberDao memberDao;
+	
+	@Override
+	public void insertMember(Member member) {
+		
+		memberDao.insertMember(member);
+		
+	}
+
+	@Override
+	public Member selectMemberByIdAndPassWd(String memberId, String passWd) {
+	
+		return memberDao.selectMemberByIdAndPassword(memberId, passWd);
+	}
+
+	
+
+
+
+}
