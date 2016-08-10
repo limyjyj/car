de32743cea09df26c05b8d341be3a29e4ce699a8
kyeongjoo <stu22@stu22-PC>
2016-08-10 19:00:50 +0900

    나당하하하

---
+++ car/src/main/java/com/car/model/service/ReservationService.java
@@ -0,0 +1,14 @@
+package com.car.model.service;
+
+import java.util.List;
+
+import com.car.model.dto.Reservation;
+
+public interface ReservationService {
+
+	
+	void insertReservation(Reservation reservation);
+
+	
+	
+}
