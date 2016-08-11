d8cfdc37838c6eef257204efdd0a5420f24bea6c
YiWonHui <xormwhr@naver.com>
2016-08-11 15:16:37 +0900

    .

--- car/src/main/java/com/car/model/service/GroupChatServiceImpl.java
+++ car/src/main/java/com/car/model/service/GroupChatServiceImpl.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import com.car.model.dao.MysqlGroupChatDao;
 import com.car.model.dto.GroupChat;
 
-
+@Service("groupChatService")
 public class GroupChatServiceImpl implements GroupChatService {
 	
 	@Autowired
