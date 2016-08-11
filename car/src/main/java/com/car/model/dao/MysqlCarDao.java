b3ee2f88b4e2efb998b78899f607d5ebcf95349a
limyj <lyjyj0131@gmail.com>
2016-08-11 15:02:00 +0900

    a

--- car/src/main/java/com/car/model/dao/MysqlCarDao.java
+++ car/src/main/java/com/car/model/dao/MysqlCarDao.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 
 import com.car.model.dto.Car;
 import com.car.model.dto.GroupChat;
 import com.car.model.mapper.CarMapper;
 
+@Repository("mysqlCarDao")
 public class MysqlCarDao {
 	@Autowired
 	@Qualifier("sqlSessionTemplate")
--- car/src/main/java/com/car/model/service/CarServiceImpl.java
+++ car/src/main/java/com/car/model/service/CarServiceImpl.java
@@ -1,15 +1,12 @@
 package com.car.model.service;
 
 import java.util.List;
-
-import org.aspectj.lang.annotation.DeclareAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.car.model.dao.CarDao;
 import com.car.model.dto.Car;
-import com.car.model.dto.GroupChat;
 
 @Service("carService")
 public class CarServiceImpl implements CarService {
@@ -22,15 +19,16 @@ public class CarServiceImpl implements CarService {
 	public void insertCar(Car car) {
 		carDao.insertCar(car);
 	}
+
 	@Override
 	public List<Car> selectCars() {
-		
+
 		return carDao.selectCars();
 	}
 
 	@Override
 	public Car selectCarByCarno(int carno) {
-		
+
 		return carDao.selectCarByCarno(carno);
 	}
 }
