344523e95fb91f1a6d5cb2348a7a47391c2fa8af
kyeongjoo <stu22@stu22-PC>
2016-08-11 21:01:40 +0900

    집에간다아아앙

--- car/src/main/java/com/car/controller/ReservationController.java
+++ car/src/main/java/com/car/controller/ReservationController.java
@@ -11,21 +11,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.car.model.dto.Reservation;
 import com.car.model.service.ReservationService;
 import com.car.ui.ThePager3;
+import com.car.controller.ReservationController;
+
 
 @Controller
 @RequestMapping("/reservation")
-public class ReservationController {
+public class ReservationController implements ApplicationContextAware, BeanNameAware{
 
+	private ApplicationContext context;
+	private String beanName;
+	
+	@Override
+	public void setBeanName(String arg0) {
+		this.beanName = arg0;
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext arg0) throws BeansException {
+		this.context = arg0;
+	}
+	
+	
 	@Autowired
 	@Qualifier("reservationService")
 	private ReservationService reservationService;
@@ -65,55 +86,19 @@ public class ReservationController {
 		return mav;
 	}
 	
-//	
-//	@RequestMapping(value = "detail.action", method = RequestMethod.GET)
-//	public ModelAndView showBoardByBoardNo(HttpServletRequest request) {
-//
-//		ModelAndView mav = new ModelAndView();
-//
-//		// 요청 정보에서 내용을 표시할 글번호를 읽고 변수에 저장
-//		// (없으면 목록으로 이동)
-//		String productNo = request.getParameter("productno");
-//		if (productNo == null || productNo.length() == 0) {
-//			System.out.println("1");
-//			mav.setViewName("redirect:/product/shopping.action");
-//			return mav;
-//		}
-//		int no = Integer.parseInt(productNo);
-//
-//		// 데이터베이스에서 데이터 조회
-//		Product product = productService.selectProductByProductNo(no);
-//
-//		// 조회 실패하면 목록으로 이동
-//		if (product == null) {
-//			System.out.println("2");
-//			mav.setViewName("redirect:/product/shopping.action");
-//			return mav;
-//		}
-//
-//		// productService.updateProductReadCount(product.getProductNo());
-//		// product.setProductCount(product.getProductCount() + 1);
-//
-//		String pageNo = "1";
-//		if (request.getParameter("pageno") != null) {
-//			pageNo = request.getParameter("pageno");
-//		}
-//
-//		List<ProductUploadFile> files = productService.selectProductuploadfileByProductNo(product.getProductNo());
-//		product.setProductUploadFiles(files);
-//
-//		for (ProductUploadFile f : files) {
-//			System.out.println("filename" + f.getProductfileName());
-//		}
-//
-//		// 조회된 데이터를 jsp 처리할 수 있도록 request 객체에 저장
-//		mav.setViewName("shopping/detail");
-//		mav.addObject("product", product);
-//		mav.addObject("pageno", pageNo);
-//
-//		return mav;
-//
-//	}
-	
+
+	//작성
+		@RequestMapping(value = "writeform.action", method = RequestMethod.GET)
+		public String getBoardWriteForm() {
+			return "board/writeform";
+		}
+
+		@RequestMapping(value = "write.action", method = RequestMethod.POST)
+		public String writeReservation(MultipartHttpServletRequest req, Reservation reservation) {
+			
+		
+			return "redirect:/reservation/list.action";
+		}
+
 	
 }
