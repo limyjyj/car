f703a9e8d48eaaaef6fac53518b1474cf7844fa9
sm <tomato869@naver.com>
2016-08-18 17:58:07 +0900

    휴

--- car/src/main/java/com/car/ui/ThePager2.java
+++ car/src/main/java/com/car/ui/ThePager2.java
@@ -1,7 +1,6 @@
 package com.car.ui;
 
-
-public class ThePager {
+public class ThePager2 {
 	
 	private int pageSize;//한 페이지당 데이터 개수
 	private int pagerSize;//번호로 보여주는 페이지 Link 개수
@@ -12,7 +11,7 @@ public class ThePager {
 	
 	private String linkUrl;//페이저가 포함되는 페이지의 주소
 	
-	public ThePager(int dataCount, int currentPage, 
+	public ThePager2(int dataCount, int currentPage, 
 		int pageSize, int pagerSize, String linkUrl) {
 		
 		this.linkUrl = linkUrl;
@@ -25,15 +24,19 @@ public class ThePager {
 			(dataCount / pageSize) + ((dataCount % pageSize) > 0 ? 1 : 0); 
 	}
 	
+	@Override
 	public String toString(){
 		StringBuffer linkString = new StringBuffer();
-		linkString.append("<ul class='pagination'>");
+		
 		//1. 처음, 이전 항목 만들기
 		if (currentPage > 1) {
 			linkString.append(
-				String.format("<li><a href='%s?pageno=1'>처음</a></li>",linkUrl));
+				String.format("[<a href='%s?pageno=1'>처음</a>]",linkUrl));
+			linkString.append("&nbsp;");
+			linkString.append("&nbsp;");
 			linkString.append(String.format(
-				"<li><a href='%s?pageno=%d'>이전</a></li>", linkUrl, currentPage - 1));
+				"[<a href='%s?pageno=%d'>이전</a>]", linkUrl, currentPage - 1));
+			linkString.append("&nbsp;");
 		}
 		
 		//2. 페이지 번호 Link 만들기
@@ -42,24 +45,27 @@ public class ThePager {
 		int end = start + pagerSize;
 		for (int i = start; i < end; i++) {
 			if (i > pageCount) break;
-			
+			linkString.append("&nbsp;");
 			if(i == currentPage) {
-				linkString.append(String.format("<li><span>%d</span></li>", i));
+				linkString.append(String.format("[%d]", i));
 			} else { 
 				linkString.append(String.format(
-					"<li><a href='%s?pageno=%d'>%d</a></li>", linkUrl, i, i));
+					"<a href='%s?pageno=%d'>%d</a>", linkUrl, i, i));
 			}
+			linkString.append("&nbsp;");
 		}
 		
 		//3. 다음, 마지막 항목 만들기
 		if (currentPage < pageCount) {
 			linkString.append("&nbsp;");
 			linkString.append(String.format(
-				"<li><a href='%s?pageno=%d'>다음</a></li>",linkUrl, currentPage + 1));
+				"[<a href='%s?pageno=%d'>다음</a>]",linkUrl, currentPage + 1));
+			linkString.append("&nbsp;");
+			linkString.append("&nbsp;");
 			linkString.append(String.format(
-				"<li><a href='%s?pageno=%d'>마지막</a></li>", linkUrl, pageCount));
+				"[<a href='%s?pageno=%d'>마지막</a>]", linkUrl, pageCount));
 		}
-		linkString.append("</ul>");
+		
 		return linkString.toString();
 	}
 
