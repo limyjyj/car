573e2b4412d29792b4161adb7b02fefe405eebac
YiWonHui <xormwhr@naver.com>
2016-08-19 11:40:59 +0900

    ..

--- car/src/main/java/com/car/interceptor/AuthInterceptor.java
+++ car/src/main/java/com/car/interceptor/AuthInterceptor.java
@@ -9,51 +9,51 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.car.model.dto.Member;
 
-public class AuthInterceptor implements HandlerInterceptor{
+public class AuthInterceptor implements HandlerInterceptor {
 
 	@Override
-	public boolean preHandle(
-			HttpServletRequest req, HttpServletResponse resp,
-			Object handler) throws Exception {
-		
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+
 		HttpSession session = req.getSession();
-		Member member = (Member)session.getAttribute("loginuser");
-		
-		String url = req.getRequestURI();
-		
+		Member member = (Member) session.getAttribute("loginuser");
 		boolean redirect = false;
-		
-		if(url.contains("/board/") || url.contains("/car/")
-				 || url.contains("/carhistory/") || url.contains("/fuel/")
-				 || url.contains("/groupchat/") || url.contains("/groupschedule/")
-				 || url.contains("/member/") || url.contains("/outcome/") || 
-				 url.contains("/reservation/") ){
-			if(member != null){
-			}else{
+
+		String url = req.getRequestURI();
+
+		if (url.contains("/member/")) {
+			if (member != null) {
+
+			} else {
+				redirect = true;
+			}
+		} else if (url.contains("/board/") || url.contains("/car/car/") || url.contains("/carhistory/")
+				|| url.contains("/fuel/") || url.contains("/groupchat/") || url.contains("/groupschedule/")
+				|| url.contains("/outcome/") || url.contains("/reservation/")) {
+			if (member != null) {
+			} else {
 				redirect = true;
 			}
 		}
 		
-		if(redirect){
+		if (redirect) {
 			resp.sendRedirect("/car/account/login.action");
 			return false;
-		}else{
+		} else {
 			return true;
 		}
-		
+
 	}
-	
+
 	@Override
 	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
 			throws Exception {
-	
+
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
 			throws Exception {
-	
+
 	}
 
-	
 }
